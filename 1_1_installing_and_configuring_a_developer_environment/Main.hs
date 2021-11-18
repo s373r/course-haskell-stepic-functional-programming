36d3e1e360ec0b0325238c4fc6b73a42ca105d41
Dima Pristupa <dima.pristupa@pm.me>
2021-11-18 14:20:00 +0200

    🚧 Finish 1.1.

---
+++ 1_1_installing_and_configuring_a_developer_environment/Main.hs
@@ -0,0 +1,86 @@
+{-
+Information
+===========
+
+Tools
+-----
+ GHC - compiler
+GHCi - interpreter (REPL)
+
+Source
+------
+.hs - text files
+
+Indentation
+-----------
+a tab (\t) character == 8 spaces
+
+Language version
+----------------
+Haskell 2010 is the latest version
+
+Haskell
+-------
+is a general-purpose, statically typed, purely functional programming language
+with type inference and lazy evaluation
+
+It is named after logician Haskell Curry
+
+Documentation
+-------------
+https://www.haskell.org
+https://www.haskell.org/hoogle/
+-}
+
+{-
+GHCi
+
+====
+Prelude
+-------
+is imported by default into all Haskell modules unless either there is
+an explicit import statement for it, or the NoImplicitPrelude extension
+is enabled
+
+Set prompt
+----------
+:set prompt "GHCi> "
+
+-}
+
+-- >>> 33 + 3 * 3
+-- 42
+
+-- >>> pi
+-- 3.141592653589793
+
+-- >>> "ABC" ++ "DE"
+-- "ABCDE"
+
+{-
+TASK
+====
+Какое приглашение на самом деле выдает командная строка интерпретатора
+(в предыдущем примере интерпретатор выдал приглашение GHCi> )?
+
+SOLUTION
+========
+*Main>
+-}
+
+{-
+Import in GHCi
+==============
+> :load Test -- or :l Test
+> sayHello
+Hello from module Test!
+
+Reload changed modules
+======================
+:reload [Module]
+:r      [Module]
+
+> :r Test
+> sayHello
+Hello World from module Test!
+-}
--- README.md
+++ README.md
@@ -11,8 +11,8 @@ Status: 🚧
 
 ## Index
 
-- 🚧 1. Введение / `Introduction`
-  - 🚧 1.1. Установка и настройка среды / `Installing and configuring a developer environment`
+- 1. Введение / `Introduction`
+  - [✏️ 1.1. Установка и настройка среды](1_1_installing_and_configuring_a_developer_environment/Main.hs) / `Installing and configuring a developer environment`
   - 🚧 1.2. Функции / `Functions`
   - 🚧 1.3. Операторы / `Operators`
   - 🚧 1.4. Базовые типы / `Basic types`
